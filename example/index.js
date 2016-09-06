f74b047ceb96c86c34af450c93141478a6e6e29e
qiaolb <qiaolibo@126.com>
2016-09-07 07:16:01 +0800

    增加例子

---
+++ example/index.js
@@ -0,0 +1,34 @@
+/**
+ * Created by joe on 16/9/7.
+ */
+
+import React from 'react';
+import DragScroll from '../lib/DragScroll';
+
+class Main extends React.Component {
+  render () {
+    <DragScroll height={200} width={300}>
+      <div>test1</div>
+      <div>test2</div>
+      <div>test3</div>
+      <div>test4</div>
+      <div>test5</div>
+      <div>test6</div>
+      <div>test7</div>
+      <div>test8</div>
+      <div>test9</div>
+      <div>test10</div>
+      <div>test11</div>
+      <div>test12</div>
+      <div>test13</div>
+      <div>test14</div>
+      <div>test15</div>
+      <div>test16</div>
+      <div>test17</div>
+      <div>test18</div>
+      <div>test19</div>
+      <div>test20</div>
+      <div>test21</div>
+    </DragScroll>;
+  }
+}
