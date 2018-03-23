504c7a160b297c7de39819f378dbb72656b7750a
Charles Poon <poonchun@hket.com>
2018-03-23 11:08:09 +0800

    rmv unnecessary syntaxes; put all props into elements; spread syntaxes for styles

--- src/DragScroll.jsx
+++ src/DragScroll.jsx
@@ -4,86 +4,87 @@
 
 import React from "react";
 
-export default class DragScroll extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: props.dataSource,
-      dragging: false
+class DragScroll extends React.Component {
+    state = {
+        data: props.dataSource,
+        dragging: false
     };
-  }
 
-  render() {
-    let sytle = null;
-    if (this.props.height && this.props.width) {
-      sytle = {style: {height: this.props.height, width: this.props.width, overflow: 'auto'}};
+    componentDidMount() {
+        window.addEventListener('mouseup', this.mouseUpHandle.bind(this));
+        window.addEventListener('mousemove', this.mouseMoveHandle.bind(this));
     }
-    return <div className={this.props.className} {...sytle}
+
+    render() {
+        let {
+            children,
+            style,
+            ...etcProps,
+        } = this.props;
+
+        if (this.props.height && this.props.width) {
+            sytle = {
+                ...style,
+                height: this.props.height,
+                width: this.props.width,
+                overflow: 'auto',
+            };
+        }
+        return (
+            <div
+                {...etcProps}
+                style={style}
                 onMouseUp={this.mouseUpHandle.bind(this)}
                 onMouseMove={this.mouseMoveHandle.bind(this)}
-                ref="container">
-      {this.props.children && this.renderChildren(this.props.children)}
-    </div>;
-  }
-
-  componentDidMount() {
-    window.addEventListener('mouseup', this.mouseUpHandle.bind(this));
-    window.addEventListener('mousemove', this.mouseMoveHandle.bind(this));
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('mouseup', this.mouseUpHandle.bind(this));
-    window.removeEventListener('mousemove', this.mouseMoveHandle.bind(this));
-  }
-
-  mouseUpHandle(e) {
-    if (this.state.dragging) {
-      this.state.dragging = false;
-      this.setState(this.state);
+                ref={(r) => this.container = r}
+            >
+                {children && this.renderChildren(children)}
+            </div>
+        );
     }
-  }
 
-  mouseDownHandle(e) {
-    if (!this.state.dragging) {
-      this.state.dragging = true;
-      this.setState(this.state);
-      this.lastClientX = e.clientX;
-      this.lastClientY = e.clientY;
-      e.preventDefault();
+    renderChildren(e) {
+        return React.Children.map(e, (item, index) => 
+            React.cloneElement(
+                item,
+                {
+                    ...item.props,
+                    key: item.key || index,
+                    onMouseUp: this.mouseUpHandle.bind(this),
+                    onMouseDown: this.mouseDownHandle.bind(this)
+                }
+            )
+        );
     }
-  }
 
-  mouseMoveHandle(e) {
-    if (this.state.dragging) {
-      this.refs.container.scrollLeft -=
-        (-this.lastClientX + (this.lastClientX = e.clientX));
-      this.refs.container.scrollTop -=
-        (-this.lastClientY + (this.lastClientY = e.clientY));
+    componentWillUnmount() {
+        window.removeEventListener('mouseup', this.mouseUpHandle.bind(this));
+        window.removeEventListener('mousemove', this.mouseMoveHandle.bind(this));
     }
-  }
 
-  renderChildren(dom, type) {
-    if (this.isArray(dom)) {
-      return dom.map((item, index) => {
-        return React.cloneElement(item, {
-          key: item.key || index,
-          onMouseUp: this.mouseUpHandle.bind(this),
-          onMouseDown: this.mouseDownHandle.bind(this)
-        });
-      });
-    } else if ('object' == typeof dom) {
-      return React.cloneElement(dom, {
-        onMouseUp: this.mouseUpHandle.bind(this),
-        onMouseDown: this.mouseDownHandle.bind(this)
-      });
+    mouseUpHandle(e) {
+        if (this.state.dragging) {
+            this.setState({ dragging: false });
+        }
     }
-  }
 
-  isArray(object){
-    return  object && typeof object==='object' &&
-      typeof object.length==='number' &&
-      typeof object.splice==='function' &&
-      //判断length属性是否是可枚举的 对于数组 将得到false
-      !(object.propertyIsEnumerable('length'));
-  }
+    mouseDownHandle(e) {
+        e.preventDefault();
+        if (!this.state.dragging) {
+            this.setState({ dragging: false });
+            this.lastClientX = e.clientX;
+            this.lastClientY = e.clientY;
+        }
+    }
+
+    mouseMoveHandle(e) {
+        if (this.state.dragging) {
+            this.container.scrollLeft -=
+                (-this.lastClientX + (this.lastClientX = e.clientX));
+            this.container.scrollTop -=
+                (-this.lastClientY + (this.lastClientY = e.clientY));
+        }
+    }
 }
+
+export default DragScroll;
